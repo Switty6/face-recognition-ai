681b0304061ed789e174b9d13097b1159bb3d24b
Switty6 <mihai.trandafir0306@gmail.com>
2021-10-28 01:00:57 +0300

    A.I training, importing libs, setup.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,72 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="column items-center">
+        <video class="col" id="video" ref="video" width="720" height="560" autoplay></video>
+    </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+    margin: 0;
+    padding: 0;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+canvas {
+    position: absolute;
 }
 </style>
+<script>
+import * as faceapi from 'face-api.js';
+
+export default {
+    data() {
+        return {
+            video: {},
+        }
+    },
+    methods: {
+        startVideo() {
+            this.video = this.$refs.video;
+            // video: ElementRef;
+            if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+                navigator.mediaDevices
+                    .getUserMedia({ video: true })
+                    .then((stream) => {
+                        // this.video.src = window.URL.createObjectURL(stream)
+                        this.video.srcObject = stream;
+                        // this.video.nativeElement.play();
+                    })
+                    .catch((err) => alert(`Bummer! ${err.name}.`));
+            }
+        }
+    },
+    mounted() {
+        const video = this.$refs.video
+        Promise.all([
+            faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+            faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+            faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+            faceapi.nets.faceExpressionNet.loadFromUri('/models')
+        ]).then(this.startVideo())
+        video.addEventListener('play', () => {
+            const canvas = faceapi.createCanvasFromMedia(video)
+            document.body.append(canvas)
+            const displaySize = { width: video.width, height: video.height }
+            faceapi.matchDimensions(canvas, displaySize)
+            setInterval(async () => {
+                const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+                const resizedDetections = faceapi.resizeResults(detections, displaySize)
+                canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+                faceapi.draw.drawDetections(canvas, resizedDetections)
+                faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+                faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+            }, 100)
+        })
+    }
+}
+
+</script>
---
+++ src/starter.vue
@@ -0,0 +1,10 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import * as faceapi from 'face-api.js'
+
+navigator.mediaDevices.getUserMedia(constraints)
+
+</script>
